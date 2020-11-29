83134276e51784dd331c7fe4b3aecf170d592348
daooda <davinhoba9@outlook.fr>
2020-11-29 02:54:43 +0000

    adding Career page ✔

---
+++ pages/career.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+
+    <div class="overflow-x-hidden">
+      <div class="projects container-inner mx-auto text-xl border-t border-gray-500 border-b py-16 mb-16 relative">
+        <h2 class="font-bold mb-6">Here are some projects I've worked on:</h2>
+
+        <div class="absolute right-0" style="top: 50px; transform: translate(100%) rotate(180deg)">
+          <svg width="170px" height="170px"><use xlink:href="#dots-triangle" /></svg>
+        </div>
+
+        <ul class="text-lg sm:text-xl space-y-6">
+            
+          <li class="checkmark">
+            <div>Taarú</div>
+            <div class="text-lg text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ab nam animi iure nemo exercitationem a, eius non culpa itaque! <br>
+            <a href="https://marketplace.visualstudio.com/items?itemName=daoodaba975.taaru" target="_blank">Visit website</a> </div>
+          </li>
+          
+        </ul>
+      </div> <!-- end projects -->
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
