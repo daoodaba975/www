2123e82c785239de7afe56678ed5cac7cd8fb783
daooda <davinhoba9@outlook.fr>
2020-11-30 01:22:13 +0000

    update ✔

--- pages/index.vue
+++ pages/index.vue
@@ -41,10 +41,10 @@
       </div> <!-- end get-to-know me -->
     </div>
 
-    <div class="overflow-x-hidden">
+    <div class="overflow-x-hidden" id="contact">
       <div class="contact-me bg-background-secondary pt-16">
         <div class="container-inner mx-auto text-xl pb-4 relative">
-          <h2 class="font-bold mb-6" id="contact">Contact me:</h2>
+          <h2 class="font-bold mb-6">Contact me:</h2>
 
           <div class="absolute right-0 top-0" style="transform: translate(100%) rotate(180deg)">
             <svg width="170px" height="170px"><use xlink:href="#dots-triangle" /></svg>
