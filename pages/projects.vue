28880f9d519ab2858dc58567710c20be606e6371
daoodaba975 <davinhoba9@outlook.fr>
2021-10-11 20:58:10 +0000

    updating projects ✔️

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,56 +1,69 @@
 <template>
   <div>
-
     <div class="overflow-x-hidden">
       <div class="projects container-inner mx-auto text-xl border-t border-gray-500 border-b py-16 mb-16 relative">
-        <h2 class="font-bold mb-6">Here are some projects I've worked on:</h2>
-
+        
+        <h2 class="font-bold mb-6 underline">Here are some projects I've worked on for client :</h2>
         <div class="absolute right-0" style="top: 50px; transform: translate(100%) rotate(180deg)">
           <svg width="170px" height="170px"><use xlink:href="#dots-triangle" /></svg>
         </div>
-
         <ul class="text-lg sm:text-xl space-y-6">
-            
-          <li class="checkmark">
-            <div>🚀 Taarú</div>
-            <div class="text-lg text-gray-600">A nice theme with Senegalese beauty, for coding the world.<br>
-            <a href="https://marketplace.visualstudio.com/items?itemName=daoodaba975.taaru" target="_blank">Learn more</a> </div>
-          </li>
-          <li class="checkmark">
-            <div>🇸🇳 Made In Senegal</div>
-            <div class="text-lg text-gray-600">A collection of tools built by Senegalese developers that can be used by anyone all over the world.<br>
-            <a href="https://madein.galsendev.com/" target="_blank">Visit website</a> </div>
-          </li>
-          <li class="checkmark">
-            <div>👨🏽‍💻 Galsen Dev</div>
-            <div class="text-lg text-gray-600">The official website of Galsen Dev community.<br>
-            <a href="https://galsendev.com/" target="_blank">Visit website</a> </div>
-          </li>
-          <li class="checkmark">
-            <div>⚠ Xex Corona</div>
-            <div class="text-lg text-gray-600">Web app for the eradication of the virus (Covid19) in Senegal.<br>
-            <a href="https://github.com/DevOlutionLab/xex-corona-app" target="_blank">Visit website</a> </div>
-          </li>
           <li class="checkmark">
             <div>🚆 ALD Caténaire</div>
             <div class="text-lg text-gray-600">ALD Caténaire is a company based in France and Senegal which works in railway infrastructure.<br>
             <a href="https://ald-catenaire.com/" target="_blank">Visit website</a> </div>
           </li>
           <li class="checkmark">
-            <div>📚 Exigence School</div>
-            <div class="text-lg text-gray-600">Requirement School is software developed to facilitate the automation of the education sector.<br>
-            <a href="https://exigenceschool.com" target="_blank">Visit website</a> </div>
-          </li>
-          <li class="checkmark">
             <div>🎨 Omana</div>
             <div class="text-lg text-gray-600">Omana is a platform to promote made in Senegal, while supporting artisans to sell their products.<br>
             <a href="https://omana.sn" target="_blank">Visit website</a> </div>
           </li>
-
         </ul>
+
+        <h2 class="font-bold mb-6 mt-12 underline">Some open-source projects I've worked on :</h2>
+        <div class="absolute right-0" style="top: 50px; transform: translate(100%) rotate(180deg)">
+          <svg width="170px" height="170px"><use xlink:href="#dots-triangle" /></svg>
+        </div>
+        <ul class="text-lg sm:text-xl space-y-6">
+          <li class="checkmark">
+            <div>🚀 Taarú</div>
+            <div class="text-lg text-gray-600">A nice theme with Senegalese beauty, for coding the world.<br>
+            <a href="https://marketplace.visualstudio.com/items?itemName=daoodaba975.taaru" target="_blank">Learn more</a> </div>
+          </li>
+          <li class="checkmark">
+            <div>🌟 Made In Senegal</div>
+            <div class="text-lg text-gray-600">A collection of tools built by Senegalese developers that can be used by anyone all over the world.<br>
+            <a href="https://madein.galsendev.com" target="_blank">Visit website</a> </div>
+          </li>
+          <li class="checkmark">
+            <div>🦠 Xex Corona</div>
+            <div class="text-lg text-gray-600">Web app for the eradication of the virus (Covid19) in Senegal.<br>
+            <a href="https://github.com/DevOlutionLab/xex-corona-app" target="_blank">Visit website</a> </div>
+          </li>
+          <li class="checkmark">
+            <div>📺 Senegal YouTuber DevList</div>
+            <div class="text-lg text-gray-600">A list of Senegalese development channels on YouTube.<br>
+            <a href="https://github.com/daoodaba975/senegal-YouTuber-Dev-List" target="_blank">Learn more</a> </div>
+          </li>
+          <li class="checkmark">
+            <div>🧩 Le Bilan</div>
+            <div class="text-lg text-gray-600">A basic BATCH program that allows you to calculate your points when playing Scrabble.<br>
+            <a href="https://github.com/daoodaba975/bilan" target="_blank">Visit website</a> </div>
+          </li>
+          <li class="checkmark">
+            <div>🛠 Basics Frontend components collections</div>
+            <div class="text-lg text-gray-600">This is a large collections of some basics frontend components crafted with HTML, CSS & JavaScript only, ready to be used.<br>
+            <a href="https://github.com/daoodaba975/basics-frontend-components-collections" target="_blank">Visit website</a> </div>
+          </li>
+          <li class="checkmark">
+            <div>💡 WEB project generator with GO</div>
+            <div class="text-lg text-gray-600">This is a basic folder generator for your web projects, made with GO.<br>
+            <a href="https://github.com/daoodaba975/WEB-project-generator-with-GO" target="_blank">Visit website</a> </div>
+          </li>
+        </ul>
+
       </div> <!-- end projects -->
     </div>
-
   </div>
 </template>
 
