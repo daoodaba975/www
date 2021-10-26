19364d493361e8d9854bbe97748bf69e809a7116
daoodaba975 <davinhoba9@outlook.fr>
2021-10-26 22:39:33 +0000

    Refacto ✅

--- layouts/default.vue
+++ layouts/default.vue
@@ -27,8 +27,11 @@
             <nuxt-link to="/career" class="text-copy-primary hover:text-gray-600">Career</nuxt-link>
           </li>
           <li>
-            <a v-if="$route.path === '/'" href="/#contact" v-scroll-to="'#contact'" class="text-copy-primary hover:text-gray-600">Contact</a>
-            <nuxt-link v-else to="/#contact" v-scroll-to="'/#contact'" class="text-copy-primary hover:text-gray-600">Contact</nuxt-link>
+            <nuxt-link to="/contact" class="text-copy-primary hover:text-gray-600">Contact</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/" class="text-copy-primary hover:text-gray-600">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg></nuxt-link>
           </li>
         </ul>
       </nav>
