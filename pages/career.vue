b14a15309804d9d84bb4fcf6abff16ed3cba612f
daoodaba975 <davinhoba9@outlook.fr>
2021-11-08 21:12:57 +0000

    Fix little typo.

--- pages/career.vue
+++ pages/career.vue
@@ -4,7 +4,7 @@
     <div class="overflow-x-hidden">
       <div class="projects container-inner mx-auto text-xl py-16 mb-16 relative">
         
-        <h2 class="font-bold mb-6 underline">I had the opportunity to work with different structures throughout my career, here is a brief summary of my background: </h2>
+        <h2 class="font-bold mb-6 underline">I had the opportunity to work with different structures, here is a brief summary:</h2>
         <div class="absolute right-0" style="top: 50px; transform: translate(100%) rotate(180deg)">
           <svg width="170px" height="170px"><use xlink:href="#dots-triangle" /></svg>
         </div>
@@ -27,7 +27,7 @@
           </li>
         </ul>
 
-        <h2 class="font-bold mb-6 mt-12 underline">My training course as a developer was a very good experience to train me, here it is: </h2>
+        <h2 class="font-bold mb-6 mt-12 underline">My training course as a developer was a very good experience, here it is:</h2>
         <div class="absolute right-0" style="top: 50px; transform: translate(100%) rotate(180deg)">
           <svg width="170px" height="170px"><use xlink:href="#dots-triangle" /></svg>
         </div>
