8be7d043e4f02ad73b28fcf0c12f8e9d886a5676
daoodaba975 <davinhoba9@outlook.fr>
2021-11-09 16:52:27 +0000

    switch Home & Theme buttons 🔄

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,9 +19,6 @@
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg></nuxt-link>
           </li>
           <li>
-            <theme-switcher :theme="theme" @themeChanged="updateTheme" />
-          </li>
-          <li>
             <nuxt-link to="/blog" class="text-copy-primary hover:text-gray-600">Blog</nuxt-link>
           </li>
           <li>
@@ -32,6 +29,9 @@
           </li>
           <li>
             <nuxt-link to="/contact" class="text-copy-primary hover:text-gray-600">Contact</nuxt-link>
+          </li>
+          <li>
+            <theme-switcher :theme="theme" @themeChanged="updateTheme" />
           </li>
         </ul>
       </nav>
