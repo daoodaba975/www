fc01650a949fedbeca4e48236cc4c29a01b253e1
daoodaba975 <davinhoba9@outlook.fr>
2022-02-23 15:49:46 +0000

    Career page ✔️

---
+++ pages/career.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+
+    <div class="overflow-x-hidden">
+      <div class="projects container-inner mx-auto text-xl py-16 mb-16 relative">
+        
+        <h2 class="font-bold mb-6 underline">I had the opportunity to work with different structures, here is a brief summary:</h2>
+        <div class="absolute right-0" style="top: 50px; transform: translate(100%) rotate(180deg)">
+          <svg width="170px" height="170px"><use xlink:href="#dots-triangle" /></svg>
+        </div>
+        <ul class="text-lg sm:text-xl space-y-6">            
+          <li class="code">
+            <div>Fullstack Software Engineer</div>
+            <div class="text-lg text-gray-600">@ <a class="underline" href="https://www.norma.dev/" target="_blank">Norma</a> (Remote, full time) September 2021 – January 2022 – Dakar, Senegal</div>
+          </li>
+          <li class="code">
+            <div>Developer Web</div>
+            <div class="text-lg text-gray-600">@ <a class="underline" href="https://turingconsulting.fr/" target="_blank">Turing Consulting France</a> (Remote, full time) August 2020 – September 2021 – Dakar, Senegal</div>
+          </li>
+          <li class="code">
+            <div>Developer Frontend</div>
+            <div class="text-lg text-gray-600">@ <span class="underline" href="#">Baobab TS</span> (Traineeship) October 2018 – February 2019 – Dakar, Senegal</div>
+          </li>
+          <li class="code">
+            <div>Fullstack Developer</div>
+            <div class="text-lg text-gray-600">Freelance – Dakar, Senegal</div>
+          </li>
+        </ul>
+
+        <h2 class="font-bold mb-6 mt-12 underline">My training as a developer was a key step for the rest of my career, here it is:</h2>
+        <div class="absolute right-0" style="top: 50px; transform: translate(100%) rotate(180deg)">
+          <svg width="170px" height="170px"><use xlink:href="#dots-triangle" /></svg>
+        </div>
+        <ul class="text-lg sm:text-xl space-y-6">            
+          <li class="graduation">
+            <div>Web & Mobile development</div>
+            <div class="text-lg text-gray-600">Professional Certificate of Specialization in Web & Mobile Development @ <a class="underline" href="https://www.academysonatel.com/" target="_blank">Sonatel Academy (ODC)</a> (2019 – 2020) Dakar, Senegal</div>
+          </li>
+          <li class="graduation">
+            <div>Software Engineering</div>
+            <div class="text-lg text-gray-600">License @ <a class="underline" href="https://www.groupeisi.com/" target="_blank">Institut Supérieur de l'Informatique (ISI)</a> (2016 – 2019) Dakar, Senegal</div>
+          </li>
+        </ul>
+
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
