ab5fa7aadfc5f45042103cddd1d20ea234e0ede7
daoodaba975 <davinhoba9@outlook.fr>
2022-09-08 23:48:20 +0000

    LanguageSwitch components ✅

---
+++ components/LanguageSwitcher.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <nuxt-link v-if="$i18n.locale !== 'en'" :to="switchLocalePath('en')">
+      EN
+    </nuxt-link>
+    <nuxt-link v-if="$i18n.locale !== 'fr'" :to="switchLocalePath('fr')">
+      FR
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LanguageSwitcher',
+}
+</script>
