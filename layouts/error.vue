93cd371dce9e06e029f88831be9230393465ddd1
daoodaba975 <davinhoba9@outlook.fr>
2022-09-16 23:58:25 +0000

    new translation ✅

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="container-inner mx-auto py-16">
     <div v-if="error.statusCode === 404">
-      <h2 class="text-4xl font-bold mb-16">Page Not Found</h2>
+      <h2 class="text-4xl font-bold mb-16">{{ $t('error1') }}</h2>
       <img src="~/assets/img/unDraw/404.svg" height="" width="" alt="404 page not foud"/>
     </div>
     <div v-else>
-      <h2>An error occured</h2>
+      <h2>{{ $t('error2') }}</h2>
     </div>
   </div>
 </template>
