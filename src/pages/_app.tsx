24bb6e5ac73df2de1183817a9980e676521ce3e8
daoodaba975 <davinhoba9@outlook.fr>
2023-04-24 23:36:58 +0000

    🔁 :: update.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'next-themes'
+
+import { Advent_Pro } from 'next/font/google'
+const adventPro = Advent_Pro({ subsets: ['latin'] })
+
+import Layout from './components/Layout'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<main className={adventPro.className}>
+				<Layout>
+					<ThemeProvider attribute="class">
+						<Component {...pageProps} />
+					</ThemeProvider>
+				</Layout>
+			</main>
+		</>
+	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     // using `src` directory :
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark': '#080818',
+        'light': '#d9d9d9',
+        'primary': '#7796ff',
+        'secondary': '#fea36d',
+      },
+    },
   },
   plugins: [],
 }
