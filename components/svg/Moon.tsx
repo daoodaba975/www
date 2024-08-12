b3f82641adb6fd491b153474569ec8f9a9f311a9
daoodaba975 <davinhoba9@outlook.fr>
2024-08-12 01:47:12 +0000

    [FEAT]: Creating all SVG components ⚡

---
+++ components/svg/Moon.tsx
@@ -0,0 +1,17 @@
+const Moon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    stroke="#25262b"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="shrink-0 size-4"
+  >
+    <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
+  </svg>
+);
+export default Moon;
