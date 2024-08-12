5c30edb999a892e37f2435808eab2b0ac4257979
daoodaba975 <davinhoba9@outlook.fr>
2024-08-12 01:40:06 +0000

    [FEAT]: Creating page routing ⚡

---
+++ routes/index.ts
@@ -0,0 +1,4 @@
+export const HomePath: string = "/";
+export const CareerPath: string = "/career";
+export const ProjectsPath: string = "/projects";
+export const ArticlesPath: string = "/articles";
