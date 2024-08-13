b937df50981a0b8662bf19423a00aea6997548e6
daoodaba975 <davinhoba9@outlook.fr>
2024-08-13 03:08:20 +0000

    [FEAT]: Add toggle button for mobile ⚡

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
@@ -7,6 +8,11 @@ import { HomePath, CareerPath, ProjectsPath, ArticlesPath } from "@/../routes";
 
 const Navbar = () => {
   const currentPath = usePathname();
+
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
 
   return (
     <header className="sticky top-0 inset-x-0 flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm backdrop-filter backdrop-blur-lg bg-opacity-30">
@@ -29,59 +35,62 @@ const Navbar = () => {
           <div className="md:hidden">
             <button
               type="button"
-              className="hs-collapse-toggle flex justify-center items-center size-6 border border-silver text-raisin rounded-full hover:bg-gray-200 focus:outline-none focus:bg-gray-200 dark:border-raisin dark:text-snow dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-              id="hs-navbar-header-floating-collapse"
-              aria-expanded="false"
-              aria-controls="hs-navbar-header-floating"
+              className="flex justify-center items-center size-6 border border-silver text-raisin rounded-full focus:outline-none dark:border-raisin dark:text-snow dark:hover:bg-silver dark:focus:bg-silver"
+              onClick={toggleMenu}
+              aria-expanded={menuOpen}
+              aria-controls="navbar-menu"
               aria-label="Toggle navigation"
-              data-hs-collapse="#hs-navbar-header-floating"
             >
-              <svg
-                className="hs-collapse-open:hidden shrink-0 size-3.5"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <line x1="3" x2="21" y1="6" y2="6" />
-                <line x1="3" x2="21" y1="12" y2="12" />
-                <line x1="3" x2="21" y1="18" y2="18" />
-              </svg>
-              <svg
-                className="hs-collapse-open:block hidden shrink-0 size-4"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M18 6 6 18" />
-                <path d="m6 6 12 12" />
-              </svg>
+              {menuOpen ? (
+                <svg
+                  className="shrink-0 size-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M18 6 6 18" />
+                  <path d="m6 6 12 12" />
+                </svg>
+              ) : (
+                <svg
+                  className="shrink-0 size-3.5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <line x1="3" x2="21" y1="6" y2="6" />
+                  <line x1="3" x2="21" y1="12" y2="12" />
+                  <line x1="3" x2="21" y1="18" y2="18" />
+                </svg>
+              )}
             </button>
           </div>
         </div>
 
         <div
-          id="hs-navbar-header-floating"
-          className="hidden hs-collapse overflow-hidden transition-all duration-300 basis-full grow md:block"
-          aria-labelledby="hs-navbar-header-floating-collapse"
+          id="navbar-menu"
+          className={`overflow-hidden transition-all duration-300 md:block ${
+            menuOpen ? "block" : "hidden"
+          }`}
         >
           <div className="flex flex-col md:flex-row md:items-center md:justify-end gap-2 md:gap-3 mt-3 md:mt-0 py-2 md:py-0 md:ps-7">
             <Link
               className={
                 currentPath === HomePath
                   ? "py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 md:border-b-4 border-raisin font-extrabold text-night hover:text-raisin focus:outline-none dark:border-snow dark:text-snow"
-                  : "py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 border-transparent text-raisin hover:text-celtic focus:outline-none dark:text-silver dark:hover:text-emerald"
+                  : "py-0.5 md:py-3 px-4 md:px-1 md:border-s-0 border-transparent text-raisin hover:text-celtic focus:outline-none dark:text-silver dark:hover:text-emerald"
               }
               href={HomePath}
               aria-current="page"
@@ -92,7 +101,7 @@ const Navbar = () => {
               className={
                 currentPath === CareerPath
                   ? "py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 md:border-b-4 border-raisin font-extrabold text-night hover:text-raisin focus:outline-none dark:border-snow dark:text-snow"
-                  : "py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 border-transparent text-raisin hover:text-celtic focus:outline-none dark:text-silver dark:hover:text-emerald"
+                  : "py-0.5 md:py-3 px-4 md:px-1 md:border-s-0 border-transparent text-raisin hover:text-celtic focus:outline-none dark:text-silver dark:hover:text-emerald"
               }
               href={CareerPath}
             >
@@ -102,7 +111,7 @@ const Navbar = () => {
               className={
                 currentPath === ProjectsPath
                   ? "py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 md:border-b-4 border-raisin font-extrabold text-night hover:text-raisin focus:outline-none dark:border-snow dark:text-snow"
-                  : "py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 border-transparent text-raisin hover:text-celtic focus:outline-none dark:text-silver dark:hover:text-emerald"
+                  : "py-0.5 md:py-3 px-4 md:px-1 md:border-s-0 border-transparent text-raisin hover:text-celtic focus:outline-none dark:text-silver dark:hover:text-emerald"
               }
               href={ProjectsPath}
             >
@@ -112,7 +121,7 @@ const Navbar = () => {
               className={
                 currentPath === ArticlesPath
                   ? "py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 md:border-b-4 border-raisin font-extrabold text-night hover:text-raisin focus:outline-none dark:border-snow dark:text-snow"
-                  : "py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 border-transparent text-raisin hover:text-celtic focus:outline-none dark:text-silver dark:hover:text-emerald"
+                  : "py-0.5 md:py-3 px-4 md:px-1 md:border-s-0 border-transparent text-raisin hover:text-celtic focus:outline-none dark:text-silver dark:hover:text-emerald"
               }
               href={ArticlesPath}
             >
