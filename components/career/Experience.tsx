a02a2ddf494250ef1491b6c1611490b1475fa50e
daoodaba975 <davinhoba9@outlook.fr>
2025-06-08 23:33:30 +0000

    [REFACTOR]: Update Experience, to correct job titles and improve layout styles 🧩

--- components/career/Experience.tsx
+++ components/career/Experience.tsx
@@ -10,7 +10,7 @@ const Experience = () => {
 
       <div>
         <div className="group relative flex gap-x-5">
-          <div className="relative group-last:after:hidden after:absolute after:top-8 after:bottom-2 after:start-3 after:w-px after:-translate-x-[0.5px]">
+          <div className="relative group-last:after:hidden after:absolute after:top-8 after:bottom-2 after:start-3 after:w-px after:-translate-x-[0.5px] after:bg-silver dark:after:bg-gayvin">
             <div className="relative z-10 size-6 flex justify-center items-center">
               <Image
                 src="/img/career/experience/turing.webp"
@@ -21,73 +21,19 @@ const Experience = () => {
               />
             </div>
           </div>
-
           <div className="grow pb-8 group-last:pb-0">
-            <h3 className="mb-1 text-xs text-night dark:text-snow">
-              @ Turing Consulting Group (Remote, full time) - since November
-              2022
-            </h3>
-
             <p className="font-semibold text-sm text-eigengrau dark:text-snow">
               FullStack Developer
             </p>
-
-            <p className="mt-1 text-sm text-night dark:text-snow">
-              I specialize in designing, developing, and maintaining websites
-              and mobile applications using technologies like JavaScript,
-              TypeScript, and Dart. My focus is on creating interactive, dynamic
-              features and optimizing performance to ensure fast loading times
-              and a smooth user experience. I prioritize responsive design to
-              guarantee compatibility across various devices and screen sizes.
-            </p>
-
-            <p className="mt-1 text-sm text-night dark:text-snow">
-              In addition to development, I am responsible for quality
-              assurance, ensuring high reliability through rigorous testing
-              procedures. I use tools such as WebDriver, Playwright, and Cypress
-              to enhance the robustness and performance of the applications,
-              ensuring they meet the highest standards.
+            <p className="mb-1 text-xs text-night dark:text-snow">
+              @ Turing Consulting Group (Remote, full time) - since November
+              2022
             </p>
           </div>
         </div>
 
         <div className="group relative flex gap-x-5">
-          <div className="relative group-last:after:hidden after:absolute after:top-8 after:bottom-2 after:start-3 after:w-px after:-translate-x-[0.5px]">
-            <div className="relative z-10 size-6 flex justify-center items-center">
-              <Image
-                src="/img/career/experience/ciga.webp"
-                className="shrink-0 size-6 rounded"
-                width={32}
-                height={32}
-                alt="Ciga logo"
-              />
-            </div>
-          </div>
-
-          <div className="grow pb-8 group-last:pb-0">
-            <h3 className="mb-1 text-xs text-night dark:text-snow">
-              @ CIGA (Part-time) - since December 2023
-            </h3>
-
-            <p className="font-semibold text-sm text-eigengrau dark:text-snow">
-              Teacher
-            </p>
-
-            <p className="mt-1 text-sm text-night dark:text-snow">
-              At CIGA, I teach JavaScript to first and third-year university
-              students. My role is to break down complex concepts into
-              accessible lessons to ensure a solid understanding of programming
-              principles. I incorporate practical projects to reinforce learning
-              and encourage students to apply their knowledge in real-life
-              situations. I foster a supportive learning environment to help
-              them build confidence in their coding skills and prepare for
-              advanced studies.
-            </p>
-          </div>
-        </div>
-
-        <div className="group relative flex gap-x-5">
-          <div className="relative group-last:after:hidden after:absolute after:top-8 after:bottom-2 after:start-3 after:w-px after:-translate-x-[0.5px]">
+          <div className="relative group-last:after:hidden after:absolute after:top-8 after:bottom-2 after:start-3 after:w-px after:-translate-x-[0.5px] after:bg-silver dark:after:bg-gayvin">
             <div className="relative z-10 size-6 flex justify-center items-center">
               <Image
                 src="/img/career/experience/xarala.webp"
@@ -99,30 +45,39 @@ const Experience = () => {
             </div>
           </div>
           <div className="grow pb-8 group-last:pb-0">
-            <h3 className="mb-1 text-xs text-night dark:text-snow">
-              @ Xarala (Part-time) - since February 2022
-            </h3>
-
             <p className="font-semibold text-sm text-eigengrau dark:text-snow">
               Mentor
             </p>
-
-            <p className="mt-1 text-sm text-night dark:text-snow">
-              As a passionate Mentor at Xarala, I contribute to the success of
-              learners by providing in-depth support, sharing knowledge, and
-              fostering a collaborative learning environment. Xarala, a leader
-              in online education, offers advanced Bootcamps focused on emerging
-              technologies. I conduct interactive sessions, offer constructive
-              feedback, and provide personalized guidance, with a focus on
-              JavaScript and Flutter—key skills in modern software development.
-              My role is to help learners acquire practical skills and prepare
-              them to tackle the challenges of the professional world.
+            <p className="mb-1 text-xs text-night dark:text-snow">
+              @ Xarala (Online, part-time) - since February 2022
             </p>
           </div>
         </div>
 
         <div className="group relative flex gap-x-5">
-          <div className="relative group-last:after:hidden after:absolute after:top-8 after:bottom-2 after:start-3 after:w-px after:-translate-x-[0.5px]">
+          <div className="relative group-last:after:hidden after:absolute after:top-8 after:bottom-2 after:start-3 after:w-px after:-translate-x-[0.5px] after:bg-silver dark:after:bg-gayvin">
+            <div className="relative z-10 size-6 flex justify-center items-center">
+              <Image
+                src="/img/career/experience/ciga.webp"
+                className="shrink-0 size-6 rounded"
+                width={32}
+                height={32}
+                alt="Ciga logo"
+              />
+            </div>
+          </div>
+          <div className="grow pb-8 group-last:pb-0">
+            <p className="font-semibold text-sm text-eigengrau dark:text-snow">
+              Teacher JavaScript / Mobile Development
+            </p>
+            <p className="mb-1 text-xs text-night dark:text-snow">
+              @ CIGA (On-site, part-time) - December 2023 / March 2025
+            </p>
+          </div>
+        </div>
+
+        <div className="group relative flex gap-x-5">
+          <div className="relative group-last:after:hidden after:absolute after:top-8 after:bottom-2 after:start-3 after:w-px after:-translate-x-[0.5px] after:bg-silver dark:after:bg-gayvin">
             <div className="relative z-10 size-6 flex justify-center items-center">
               <Image
                 src="/img/career/experience/norma.webp"
@@ -133,46 +88,18 @@ const Experience = () => {
               />
             </div>
           </div>
-
           <div className="grow pb-8 group-last:pb-0">
-            <h3 className="mb-1 text-xs text-night dark:text-snow">
-              @ Norma (Remote, full time) - September 2021 / January 2022
-            </h3>
-
             <p className="font-semibold text-sm text-eigengrau dark:text-snow">
               FullStack Software Engineer
             </p>
-
-            <p className="mt-1 text-sm text-night dark:text-snow">
-              At Norma, it was a great experience, and I learned a lot. I have
-              been involved in numerous projects, including Theolex, Yvee and
-              IndiePlaza.
+            <p className="mb-1 text-xs text-night dark:text-snow">
+              @ Norma (Remote, full time) - September 2021 / January 2022
             </p>
-
-            <ul className="list-disc ms-6 mt-3 space-y-1.5">
-              <li className="ps-1 text-sm text-night dark:text-snow">
-                Designing and developing reactive and intuitive user interfaces
-                using Flutter for mobile applications and Next.js for web
-                applications.
-              </li>
-              <li className="ps-1 text-sm text-night dark:text-snow">
-                Setting up databases and backend services using Firebase,
-                managing users, data, and authentication.
-              </li>
-              <li className="ps-1 text-sm text-night dark:text-snow">
-                Configuring Firebase to support authentication, storage,
-                real-time database, and hosting features.
-              </li>
-              <li className="ps-1 text-sm text-night dark:text-snow">
-                Implementing mobile user interfaces with Flutter, utilizing
-                custom widgets to create rich experiences.
-              </li>
-            </ul>
           </div>
         </div>
 
         <div className="group relative flex gap-x-5">
-          <div className="relative group-last:after:hidden after:absolute after:top-8 after:bottom-2 after:start-3 after:w-px after:-translate-x-[0.5px]">
+          <div className="relative group-last:after:hidden after:absolute after:top-8 after:bottom-2 after:start-3 after:w-px after:-translate-x-[0.5px] after:bg-silver dark:after:bg-gayvin">
             <div className="relative z-10 size-6 flex justify-center items-center">
               <Image
                 src="/img/career/experience/turing.webp"
@@ -183,71 +110,29 @@ const Experience = () => {
               />
             </div>
           </div>
-
           <div className="grow pb-8 group-last:pb-0">
-            <h3 className="mb-1 text-xs text-night dark:text-snow">
-              @ Turing Consulting Group (Remote, full time) - August 2020 /
-              September 2021
-            </h3>
-
             <p className="font-semibold text-sm text-eigengrau dark:text-snow">
               Web Developer
             </p>
-
-            <p className="mt-1 text-sm text-night dark:text-snow">
-              As a web developer at Turing Consulting Group, my role was to
-              craft optimized user interfaces using cutting-edge technologies
-              such as React.js and JavaScript.
+            <p className="mb-1 text-xs text-night dark:text-snow">
+              @ Turing Consulting Group (Remote, full time) - August 2020 /
+              September 2021
             </p>
-
-            <ul className="list-disc ms-6 mt-3 space-y-1.5">
-              <li className="ps-1 text-sm text-night dark:text-snow">
-                Focused on designing and enhancing responsive user experiences
-                for both mobile and web applications.
-              </li>
-              <li className="ps-1 text-sm text-night dark:text-snow">
-                Developed server-side features and configured crucial aspects
-                such as authentication, storage, real-time databases, and
-                hosting.
-              </li>
-              <li className="ps-1 text-sm text-night dark:text-snow">
-                Gained diverse experience through work on several notable
-                projects.
-              </li>
-              <li className="ps-1 text-sm text-night dark:text-snow">
-                Demonstrated the ability to make significant contributions by
-                adding new features and resolving critical issues in a dynamic
-                and innovative environment.
-              </li>
-            </ul>
           </div>
         </div>
 
         <div className="group relative flex gap-x-5">
-          <div className="relative group-last:after:hidden after:absolute after:top-8 after:bottom-2 after:start-3 after:w-px after:-translate-x-[0.5px]">
+          <div className="relative group-last:after:hidden after:absolute after:top-8 after:bottom-2 after:start-3 after:w-px after:-translate-x-[0.5px] after:bg-silver dark:after:bg-gayvin">
             <div className="relative z-10 size-6 flex justify-center items-center text-eigengrau dark:text-snow">
               <MdOutlineWorkOutline />
             </div>
           </div>
-
           <div className="grow pb-8 group-last:pb-0">
-            <h3 className="mb-1 text-xs text-night dark:text-snow">
-              Freelance - May 2018 / September 2021
-            </h3>
-
             <p className="font-semibold text-sm text-eigengrau dark:text-snow">
               FullStack Developer
             </p>
-
-            <p className="mt-1 text-sm text-night dark:text-snow">
-              As a Freelance Web & Mobile Developer, I have had the privilege of
-              collaborating with various individuals and companies, contributing
-              to a range of exciting projects. Throughout my freelance journey,
-              I have worked with diverse clients to design and implement
-              tailored solutions. This includes developing intuitive mobile
-              applications with React Native, creating dynamic websites with
-              Vue.js and Nuxt.js, as well as establishing high-performing
-              backend systems with Laravel and PHP.
+            <p className="mb-1 text-xs text-night dark:text-snow">
+              Freelance - May 2018 / September 2021
             </p>
           </div>
         </div>
@@ -258,37 +143,13 @@ const Experience = () => {
               <MdOutlineWorkOutline />
             </div>
           </div>
-
           <div className="grow pb-8 group-last:pb-0">
-            <h3 className="mb-1 text-xs text-night dark:text-snow">
-              @ Baobab TS (Traineeship) - October 2018 / February 2019
-            </h3>
-
             <p className="font-semibold text-sm text-eigengrau dark:text-snow">
               Developer Frontend
             </p>
-
-            <ul className="list-disc ms-6 mt-3 space-y-1.5">
-              <li className="ps-1 text-sm text-night dark:text-snow">
-                Design and improvement of the user interface (UI) using HTML,
-                CSS, and JavaScript.
-              </li>
-              <li className="ps-1 text-sm text-night dark:text-snow">
-                Implementation of designs and mockups provided by the design
-                team.
-              </li>
-              <li className="ps-1 text-sm text-night dark:text-snow">
-                Development of server-side features.
-              </li>
-              <li className="ps-1 text-sm text-night dark:text-snow">
-                Use of version control systems such as Git to manage and track
-                code changes.
-              </li>
-              <li className="ps-1 text-sm text-night dark:text-snow">
-                Working on specific projects assigned by the team, such as
-                adding new features and resolving critical issues.
-              </li>
-            </ul>
+            <p className="mb-1 text-xs text-night dark:text-snow">
+              @ Baobab TS (Traineeship) - October 2018 / February 2019
+            </p>
           </div>
         </div>
       </div>
