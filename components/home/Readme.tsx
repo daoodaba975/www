a7793a401f048a6984d46949db7002a335760c26
daoodaba975 <davinhoba9@outlook.fr>
2025-06-19 03:33:49 +0000

    [REFACTOR]: Refines Readme content and link styles 🧩 - improves profile introduction - removing redundant details. - simplifies dark mode link color styles

--- components/home/Readme.tsx
+++ components/home/Readme.tsx
@@ -35,21 +35,21 @@ const Readme = () => {
 
       <div className="mt-8">
         <p className="text-sm text-night dark:text-snow">
-          I am a FullStack Developer and Open Source Enthusiast from Senegal,
-          working in the field of web and mobile development. Always down for
-          building cool stuff and sharing it with the world !
+          I am a FullStack Developer and Open Source Enthusiast, working in the
+          field of web and mobile development. Always down for building cool
+          stuff !
         </p>
         <p className="text-sm text-night dark:text-snow">
-          I am the Founder & Lead of{" "}
+          I&apos;m also the founder & Lead of{" "}
           <Link
-            className="text-celtic dark:text-emerald hover:text-pacific dark:hover:text-mint"
+            className="text-celtic hover:text-pacific"
             href="https://galsen.dev"
             target="_blank"
           >
             <span className="text-sm font-bold relative inline-block stroke-current">
               Galsen DEV
               <svg
-                className="text-celtic dark:text-emerald absolute -bottom-0.5 w-full max-h-1.5"
+                className="text-celtic absolute -bottom-0.5 w-full max-h-1.5"
                 viewBox="0 0 55 5"
                 xmlns="http://www.w3.org/2000/svg"
                 preserveAspectRatio="none"
@@ -66,17 +66,11 @@ const Readme = () => {
           building innovative solution 🚀
         </p>
 
-        <p className="mt-3 text-sm text-night dark:text-snow">
-          I enjoy simplifying complex problems into beautiful, intuitive designs
-          🌟 My job is to craft a solution that is both functional and
-          user-friendly while also being visually appealing.
-        </p>
-
         <ul className="mt-5 flex flex-col gap-y-3">
           <li className="flex items-center gap-x-2.5 text-eigengrau dark:text-silver">
             <FaGithub />
             <Link
-              className="text-[13px] text-eigengrau underline-none hover:text-celtic hover:decoration-2 focus:outline-none focus:decoration-2 dark:text-snow dark:hover:text-emerald"
+              className="text-[13px] text-eigengrau underline-none hover:text-celtic hover:decoration-2 focus:outline-none focus:decoration-2 dark:text-snow dark:hover:text-pacific"
               href="https://github.com/daoodaba975"
               target="_blank"
             >
@@ -90,7 +84,7 @@ const Readme = () => {
           <li className="flex items-center gap-x-2.5 text-eigengrau dark:text-silver">
             <FaXTwitter />
             <Link
-              className="text-[13px] text-eigengrau underline hover:text-celtic hover:decoration-2 focus:outline-none focus:decoration-2 dark:text-snow dark:hover:text-emerald"
+              className="text-[13px] text-eigengrau underline hover:text-celtic hover:decoration-2 focus:outline-none focus:decoration-2 dark:text-snow dark:hover:text-pacific"
               href="https://x.com/daoodaba975"
               target="_blank"
             >
@@ -100,7 +94,7 @@ const Readme = () => {
           <li className="flex items-center gap-x-2.5 text-eigengrau dark:text-silver">
             <FaLinkedin />
             <Link
-              className="text-[13px] text-eigengrau underline hover:text-celtic hover:decoration-2 focus:outline-none focus:decoration-2 dark:text-snow dark:hover:text-emerald"
+              className="text-[13px] text-eigengrau underline hover:text-celtic hover:decoration-2 focus:outline-none focus:decoration-2 dark:text-snow dark:hover:text-pacific"
               href="https://www.linkedin.com/in/daoodaba975"
               target="_blank"
             >
