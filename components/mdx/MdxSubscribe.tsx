ef4e20a8de3bb58796f1610e2f69b29c4f9860d1
daoodaba975 <davinhoba9@outlook.fr>
2025-06-24 18:28:22 +0000

    [REFACTOR]: Update button hover color for improved consistency 🧩

--- components/mdx/MdxSubscribe.tsx
+++ components/mdx/MdxSubscribe.tsx
@@ -127,7 +127,7 @@ const MdxSubscribe = () => {
               <button
                 type="button"
                 onClick={handleCloseModal}
-                className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-md border-transparent bg-celtic text-snow hover:bg-pacific dark:text-snow hover:text-eigengrau dark:hover:text-eigengrau focus:outline-none focus:bg-celtic"
+                className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-semibold rounded-md border-transparent bg-celtic text-snow hover:bg-pacific dark:text-snow hover:text-gayvin dark:hover:text-eigengrau focus:outline-none focus:bg-celtic"
               >
                 Close
               </button>
